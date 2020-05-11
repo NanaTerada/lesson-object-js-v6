6e2902287d2b395bf6ce0e0432bd178cc2f5ba9e
n2 <7worki.nprogress@gmail.com>
2020-05-11 17:05:28 +0900

    #02 配列の要素にアクセスしてみよう

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@
     // const score3 = 40; 
 
     const scores = [80,90,40];
+    console.log(scores[1]);　//2つめの要素
+    //要素の変更
+    scores[2]=45;
     console.log(scores);
+
+    // scores = 10; //constなのでこの書き換えはできない
+
+    console.log(scores.length);
 }
 
