6c556a95f9a3389a2a60c6ae5afe1de9b0b8fd98
n2 <7worki.nprogress@gmail.com>
2020-05-11 17:47:23 +0900

    #08 forEach()を使ってみよう

--- js/main.js
+++ js/main.js
@@ -1,35 +1,13 @@
 'use strict';
-// 分割代入
+// forsach
 
 {
-   
    const scores = [80,90,40,70];
 
-//    定数にわけれる
-//    const[a,b,c,d] = scores;
-//    console.log(a);
-//    console.log(b);
-//    console.log(c);
-//    console.log(d);
-
-//    const [a,b,...others] = scores; //...レスト構文
-
-//    console.log(a);
-//    console.log(b);
-//    console.log(others);
-
-// 値の交換
-
-let x = 30;
-let y = 70;
-
-[x,y] = [y,x];
-
-console.log(x);
-console.log(y);
-
-
-
+//    scores.forEach((score) => {
+    scores.forEach((score,index) => { //要素のインデックスも表示
+       console.log(`Score${index} :${score}`);
+   });
 
 
 }
