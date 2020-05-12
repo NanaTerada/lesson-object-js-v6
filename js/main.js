659b0c5e54cf31bd4a9797d0088438812c1d3ad1
n2 <7worki.nprogress@gmail.com>
2020-05-12 14:59:05 +0900

    #27 例外処理を使ってみよう

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 'use strict';
 
 {
+// const name = 'taguchi' ;
+const name = 5;
 
- let i = 0;
 
- function showTime() {
-    console.log(new Date());
-    const timeoutId = setTimeout(showTime,1000); //指定した時間の後に1度だけ処理を実行する ※中にかくと繰り返しにできる
-    i++;
-    if(i > 2){
-        clearTimeout(timeoutId);
-    }
- }
- 
- showTime();
+try { 
+    console.log(name.toUpperCase());
+} catch (e){
+    console.log(e);
+}
+    console.log('finish');
 }
 
