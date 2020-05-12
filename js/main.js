67bf227a311ea9d963f5e4015bf968deb447b93d
n2 <7worki.nprogress@gmail.com>
2020-05-12 13:04:31 +0900

    #15 変数を代入してみよう

--- js/main.js
+++ js/main.js
@@ -1,31 +1,17 @@
 'use strict';
 
 {
+    // let x = 1;
+    // let y = x;
+    // x = 5;
+    // console.log(x); //5
+    // console.log(y); //1
+
+    let x = [1,2];
+    let y = x;
+    x[0] = 5;
+    console.log(x); //[5,2]
+    console.log(y); //[1,2]になるのでは？ｔおもうが　[5,2]　となる
     
-    const point = {
-        x: 100,
-        y:180,
-    };
-
-    // オブジェクトにはforeach使えないので。。。
-
-    // const keys = Object.keys(point); //point1のすべてのキーを配列で受けとれる
-    // keys.forEach( key => {
-    //     console.log(`key: ${key} Value: ${point[key]}`);
-    // });
-
-    // たとえば複数の座標を管理したい場合
-
-    const points = [
-        {x:30,y:20},
-        {x:10,y:50},
-        {x:40,y:40},
-    ];
-
-    console.log(points[1].y);
-
-    
-
-
 }
 
