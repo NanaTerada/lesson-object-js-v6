7ccba79fd50c07a905d0bfcccb5c682de54149b4
n2 <7worki.nprogress@gmail.com>
2020-05-12 17:49:45 +0900

    #34 クラスを拡張したい場合を考えよう

--- js/main.js
+++ js/main.js
@@ -2,32 +2,48 @@
 // クラス！！！
 {
     class Post{
-        constructor(text) {
+        constructor(text,sponsor) {
             this.text = text;
             this.likeCount = 0;
         }
 
         show() {
             console.log(`${this.text} - ${this.likeCount}likes`);
+            
     }
     like() {
         this.likeCount++;
         this.show();
     }
-// static 静的メソッド this使えない　インスタンスをつくらずに呼び出す
-    static showInfo() {
-        console.log('Post class vertion 1.0');
+
+}
+
+    class SponsorPost{
+        constructor(text,sponsor) {
+            this.text = text;
+            this.likeCount = 0;
+            this.sponsor = sponsor ;
+        }
+
+        show() {
+            console.log(`${this.text} - ${this.likeCount}likes`);
+            console.log(`...sponsored by ${this.sponsor}`);
     }
+    like() {
+        this.likeCount++;
+        this.show();
+    }
+
 }
 
 const posts = [
     new Post('jsの勉強中...'),
-    new Post('プログラミング楽しい！')
-
+    new Post('プログラミング楽しい！'),
+    new SponsorPost('分動画でマスターしよう','dotinstoll'),
 ];
 
-posts[0].like();
-Post.showInfo();
+posts[2].show();
+posts[2].like();
 
 
 }
