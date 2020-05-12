6cff66df2fed17cb82250ef282e1a59eea197802
n2 <7worki.nprogress@gmail.com>
2020-05-12 13:53:18 +0900

    #20 ランダムな整数値を作ってみよう

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
 'use strict';
 
 {
-   const scores = [10,3,9];
-   let sum = 0;
 
-   scores.forEach(score => {
-       sum += score;
-   });
+// console.log( Math.random());
 
-   const avg = sum/ scores.length;
-
-//    console.log(sum);
-//    console.log(avg);
-
-console.log(Math.floor(avg));　//小数点以下切り捨て
-console.log(Math.ceil(avg));　//小数点以下切り上げ
-console.log(Math.round(avg)); //四捨五入
-console.log(avg.toFixed(3)); 　//指定した小数点まで
-
-console.log( Math.random());
+// Math.floor(Math.random() * (max + 1 -min) + min);
+console.log( Math.floor(Math.random() * 6 + 1));
 
 
 }
