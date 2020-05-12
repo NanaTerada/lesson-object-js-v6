c85cedba3e566fef4fd1d5e6538fc5b629debc34
n2 <7worki.nprogress@gmail.com>
2020-05-12 14:49:23 +0900

    #25 setTimeout()を使ってみよう

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 'use strict';
 
 {
-    let i = 0;
 
-    function showTime(){
-        console.log(new Date());
-        i++;
-        if(i > 2) {
-            clearInterval(intervalId);
-        }
+ let i = 0;
+
+ function showTime() {
+    console.log(new Date());
+    const timeoutId = setTimeout(showTime,1000); //指定した時間の後に1度だけ処理を実行する ※中にかくと繰り返しにできる
+    i++;
+    if(i > 2){
+        clearTimeout(timeoutId);
     }
-
-    // 1秒ごとに処理を繰り返す
+ }
  
-    const intervalId　= setInterval(showTime,1000);　//関数を引数に渡す場合には（）はつけない ()をつけると帰り値を渡すことになる
-
+ showTime();
 }
 
