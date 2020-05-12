d2a63571eaa4ad75639b8aef8f1dd3ba42cf4f1b
n2 <7worki.nprogress@gmail.com>
2020-05-12 14:33:29 +0900

    #23 alert()､confirm()を使ってみよう

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
 {
-const d = new Date(2019,10); //2019/11/01  00:00:00
-d.setHours(10,20,30);
-d.setDate(31);
-d.setDate(d.getDate()+3);
-console.log(d);
+const answer =　confirm('削除しますか？');
+if(answer){
+    console.log('削除しました');
+} else {
+    console.log('キャンセルしました');
+}
 }
