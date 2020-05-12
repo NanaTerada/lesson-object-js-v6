4b4cb915e76f6bea6ddf4d5a75bd766875166b09
n2 <7worki.nprogress@gmail.com>
2020-05-12 14:42:48 +0900

    #24 setInterval()を使ってみよう

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 {
-const answer =　confirm('削除しますか？');
-if(answer){
-    console.log('削除しました');
-} else {
-    console.log('キャンセルしました');
+    let i = 0;
+
+    function showTime(){
+        console.log(new Date());
+        i++;
+        if(i > 2) {
+            clearInterval(intervalId);
+        }
+    }
+
+    // 1秒ごとに処理を繰り返す
+ 
+    const intervalId　= setInterval(showTime,1000);　//関数を引数に渡す場合には（）はつけない ()をつけると帰り値を渡すことになる
+
 }
-}
+
