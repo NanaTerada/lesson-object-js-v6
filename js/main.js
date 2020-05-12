01489278025c3ee3dc80fb0221826f8e4a35593e
n2 <7worki.nprogress@gmail.com>
2020-05-12 14:27:39 +0900

    #22 特定の日時を扱ってみよう

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
 {
-const d = new Date(); //現在日時
-// console.log(d);
-
-console.log(`${d.getMonth()+1}月${d.getDate()}日`); 
+const d = new Date(2019,10); //2019/11/01  00:00:00
+d.setHours(10,20,30);
+d.setDate(31);
+d.setDate(d.getDate()+3);
+console.log(d);
 }
