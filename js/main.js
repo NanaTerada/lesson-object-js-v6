f8817a13f72e242a058778a26a09f8e90d6f57d3
n2 <7worki.nprogress@gmail.com>
2020-05-12 16:54:04 +0900

    #31 クラスを作ってみよう

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 'use strict';
 // クラス！！！
 {
-    const posts = [
-        {
-            text:'jsの勉強中...',
-            likeCount : 0 ,
-            // show : function () {
-            //     console.log(`${this.text} - ${this.likeCount}いいね`);
-            show() {
-                console.log(`${this.text} - ${this.likeCount}いいね`);
-        },
-    },
-        {
-            text:'プログラミング楽しい！...',
-            likeCount : 0 ,  
-            show() {
-                console.log(`${this.text} - ${this.likeCount}いいね`);
-        },
-        },
-    ];
-    // show(posts[0]);
-    posts[0].show();
-    posts[1].show();
-};
+    class Post{
+        constructor(text) {
+            this.text = text;
+            this.likeCount = 0;
+        }
+
+        show() {
+            console.log(`${this.text} - ${this.likeCount}いいね`);
+    }
+}
+const posts = [
+    new Post('jsの勉強中...'),
+    new Post('プログラミング楽しい！')
+
+];
+
+posts[0].show();
+posts[1].show();
+
+}
 
