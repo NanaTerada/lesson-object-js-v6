24f5dcbe63b5090b03db3a557c6f3f9675b3668e
n2 <7worki.nprogress@gmail.com>
2020-05-12 11:15:58 +0900

    #13 オブジェクトを操作してみよう

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 'use strict';
 
 {
+    const otherProps = {
+        r : 4,
+        color : 'red',
+    }
     const point = {
         x: 100,
         y:180,
+        ...otherProps,
     };
-　　// 再代入
-    point.x = 120;
-    point['y'] = 150;
-
-    console.log(point.x);
-    console.log(point['y']);
-
-    point.z = 200;　//プロパティの追加
-    delete point.y; //プロパティの削除
-
+    
     console.log(point);
+
+    const {x,r,...others} = point;
+   console.log(x);
+   console.log(x);
+   console.log(others);
+　　
 
 
 }
